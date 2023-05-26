c51abfae600a7720d0a81ccd81c2c3f0adab7d03
Rohit Mondal <rohit.simbanic2023@gmail.com>
2023-05-26 21:41:18 +0530

    updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,16 @@
 import Container from "@mui/material/Container";
 
 import SubscriptionBox from "../components/SubscriptionBox";
+import RecipeReviewCard from "../components/RecipeReviewCard";
 
 const Home = () => {
   return (
-    <Container fixed sx={{ marginTop: "20px" }}>
-      <SubscriptionBox />
-    </Container>
+    <>
+      <Container fixed sx={{ marginTop: "20px" }}>
+        <SubscriptionBox />
+      </Container>
+      <RecipeReviewCard />
+    </>
   );
 };
 
